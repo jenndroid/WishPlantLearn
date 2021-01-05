6e6e5d666162242b6a6302daf23fdc9f2cd4b2d2
jenndroid <jennifer.sesaybarnes@gmail.com>
2021-01-05 19:04:28 +0000

    First commit

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,8 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'gkgcc=j8%$x37v@g(h*czwt*b=ksw&$a%92+@2e8!n&g83d=7u'
+
+SECRET_KEY = os.getenv("SECRET_KEY", "default_val")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
