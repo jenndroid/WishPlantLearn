99982f5f6a93cb8530c1f8719141788bade3345f
jenndroid <jennifer.sesaybarnes@gmail.com>
2021-01-06 13:18:27 +0000

    Add plant model

--- wishlist/models.py
+++ wishlist/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Plant(models.Model):
+    """A plant the user has bought or wants to buy"""
+    name = models.CharField(max_length=100)
+    image_link = models.CharField(max_length=200)
+    purchased = models.BooleanField()
+    notes = models.TextField()
+
+    def __str__(self):
+        """Return string representation of plant name"""
+        return self.name
+
