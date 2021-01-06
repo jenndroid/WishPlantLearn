aa266d161b678f7d7f05748c69a5a1e394bfc7fa
jenndroid <jennifer.sesaybarnes@gmail.com>
2021-01-06 15:14:06 +0000

    Add wishlist display, add link on homepage

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -6,6 +6,6 @@ from . import views
 app_name = 'wishlist'
 
 urlpatterns = [
-    # Wishlist 'homepage' - i.e just wishlist
+    # Display wishlist- i.e, all plants
     path('wishlist/', views.wishlist, name='wishlist')
 ]
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 
+from .models import Plant
+
 # Create your views here.
 def wishlist(request):
-    """The wishlist display page"""
-    return render(request, 'wishlist/wishlist.html')
+    """Show all plants"""
+    plants = Plant.objects.all()
+    context = {'plants' : plants}
+    return render(request, 'wishlist/wishlist.html', context)
